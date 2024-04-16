a40801b498904fc60cabcd6dbe21741c65c941fb
eddie9110 <125439183+eddie9110@users.noreply.github.com>
2024-04-16 16:26:16 +0300

    Add files via upload

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+module for authentication
+"""
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth():
+    """
+    Auth Class
+    """
+    
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """require authenication"""
+        if path is None:
+            return True
+        if excluded_paths is None:
+            return True
+        path = path + '/' if path[-1] != '/' else path
+        if path in excluded_paths:
+            return False
+        
+    
+    def authorization_header(self, request=None) -> str:
+        """ authorisation header"""
+        if request is None:
+            return None
+        if 'Authorization' not in request.headers:
+            return None
+        return request.headers['Authorization']
+        
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ current user"""
+        return None
