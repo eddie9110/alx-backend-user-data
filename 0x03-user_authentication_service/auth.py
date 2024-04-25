c840fbe75334e547d02666450ef15c00fdaf0ad0
eddie9110 <edwinmaregwa@gmail.com>
2024-04-25 07:12:46 -0700

    auth.py

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""Password hashing module"""
+
+import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
+
+
+def _hash_password(password: str) -> str:
+    """hashing function"""
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """User Registration"""
+        try:
+            self._db.find_user_by(email=email)
+        except NoResultFound:
+            user = self._db.add_user(email, _hash_password(password))
+            return user
+        else:
+            raise ValueError(f"User {email} already exists")
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Validation of login credentials"""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return False
+        return bcrypt.checkpw(password.encode(), user.hashed_password)
